a4c4ba7eccab7cf2a4e29ab48dff2de1f155b40e
Fons <lord.fontana@gmail.com>
2014-06-11 14:39:03 +0200

    sistemata chiusura socket
    Signed-off-by: Fons <lord.fontana@gmail.com>

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 import bluetooth
+import thread
 
 name="bt_server"
 target_name="siggen"
@@ -8,23 +9,35 @@ def runServer():
     serverSocket=bluetooth.BluetoothSocket(bluetooth.RFCOMM )
     port=bluetooth.PORT_ANY
     serverSocket.bind(("",port))
-    print "Listening for connections on port: ", port   
+    print "Listening for connections on port: ", port
     serverSocket.listen(2)
     port=serverSocket.getsockname()[1]
 
     #the missing piece
     bluetooth.advertise_service( serverSocket, "SampleServer",service_id = uuid,service_classes = [ uuid, bluetooth.SERIAL_PORT_CLASS ], profiles = [ bluetooth.SERIAL_PORT_PROFILE ])
-    
-    inputSocket, address=serverSocket.accept()
-    print "Got connection with" , address
+
+
     while True:
+        print "waiting connection"
+        inputSocket, address=serverSocket.accept()
+        thread.start_new_thread( handle_client, (inputSocket, address ))
+
+
+
+def handle_client(inputSocket,address):
+
+    print "Spawing thread for " + str(address)
+    try:
+        while True:
 	data=inputSocket.recv(1024)
 	if not data:
-	  break    
-	print "received [%s] \n " % data    
-    inputSocket.close()
-    time.sleep(0,001)
-    
-    serverSocket.close()  
+	  break
+	print "["+str(address)"]"+ " %s \n " % data
+    except SocketError as e:
+        print "client "+str(address)+" has disconnected"
+        inputSocket.close()
+        time.sleep(0,001)
+
+
 
 runServer()
