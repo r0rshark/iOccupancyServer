4e83b653cd93abec3377fb09e6a8f5d9fc25ddb4
Fons <lord.fontana@gmail.com>
2014-07-17 18:39:40 +0200

    sistemati script bluetooth
    Signed-off-by: Fons <lord.fontana@gmail.com>

--- bluetooth_server/start_bluetooth_server.sh
+++ bluetooth_server/start_bluetooth_server.sh
@@ -1,3 +1,9 @@
 ./start_bluetooth
-. venv/bin/activate
-python bluetooth_server.py
+RESULT=$?
+if [ $RESULT -eq 0 ]; then
+  . venv/bin/activate
+  python bluetooth_server.py
+else
+  echo "Problem in starting the bluetooth server"
+fi
+
